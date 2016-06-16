a48e8b2b7102023688450ea5e572dc905870e9aa
Caleb Jones <self@calebjones.net>
2016-06-16 10:06:27 -0400

    Draw a triangle

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 #[macro_use]
 extern crate glium;
 
+#[derive(Clone, Copy)]
+struct Vertex {
+    position: [f32; 2],
+}
+implement_vertex!(Vertex, position);
+
 fn main() {
     use glium::{DisplayBuild, Surface};
 
@@ -10,9 +16,24 @@ fn main() {
         .build_glium()
         .unwrap();
 
+    let shape = vec![
+        Vertex { position: [-1.0, -1.0] },
+        Vertex { position: [-1.0, 1.0] },
+        Vertex { position: [1.0, 1.0] },
+        // Vertex { position: [1.0, -1.0] },
+    ];
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = include_str!("vertex.glsl");
+    let fragment_shader_src = include_str!("fragment.glsl");
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
     loop {
         let mut frame = display.draw();
         frame.clear_color(0.0, 0.0, 0.0, 0.0);
+        frame.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms,
+                   &Default::default()).unwrap();
         frame.finish().expect("Succeeded drawing");
 
         for ev in display.poll_events() {
